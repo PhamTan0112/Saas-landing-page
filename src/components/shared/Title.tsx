593e8ebdc930d8e00e41b663bc3348a329d9045e
PhamTan <phamphutanxo@gmail.com>
2025-08-15 00:15:10 +0700

    Fix: Title.tsx

--- src/components/shared/Title.tsx
+++ src/components/shared/Title.tsx
@@ -4,7 +4,7 @@ interface TitleProps {
   children: React.ReactNode;
 }
 
-const Title = ({ children, className = "" }: TitleProps) => {
+const Title = ({ children }: TitleProps) => {
   return (
     <h1 className="text-heading-1 font-semibold text-2xl sm:text-3xl md:text-4xl">
       {children}
